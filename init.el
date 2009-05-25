2f0f8698667e2d16065a8d291e7ebb9cbf03f931
Justin W. Reagor <justinwr@gmail.com>
2009-05-25 13:34:27 -0400

    auto-emacs saving

--- init.el
+++ init.el
@@ -79,6 +79,7 @@
  '(erc-prompt ">")
  '(erc-user-full-name "justinwr")
  '(indent-tabs-mode nil)
+ '(line-spacing 1)
  '(menu-bar-mode t)
  '(show-paren-mode t)
  '(speedbar-vc-do-check nil)
@@ -89,24 +90,13 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil
-                :stipple nil
-                :background "Grey15"
-                :foreground "Grey"
-                :inverse-video nil
-                :box nil
-                :strike-through nil
-                :overline nil
-                :underline nil
-                :slant normal
-                :weight normal
-                :height 110
-                :width normal
-                :foundry "apple"
-                :family "Consolas")))))
+ '(default ((t (:inherit nil :stipple nil :background "Grey15" :foreground "Grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 120 :width normal :foundry "apple" :family "Consolas")))))
 
 ;;(toggle-debug-on-error)
 ;;(defun handle-shift-selection (&rest args))
 
 (provide 'init)
 ;;; init.el eof
+(put 'downcase-region 'disabled nil)
+
+(put 'upcase-region 'disabled nil)
