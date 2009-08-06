0e791fbdae1c30fe6c855d1817038566c3b820bc
Justin W. Reagor <justinwr@gmail.com>
2009-08-06 01:59:30 -0400

    Abstracted out unfinished xcode env config to a separate file

--- common/modes.el
+++ common/modes.el
@@ -7,7 +7,6 @@
 (require 'magit)
 
 (require 'yasnippet)
-(yas/initialize)
 (setq yas/root-directory (concat dotfiles-dir "/snippets"))
 (yas/load-directory yas/root-directory)
 
@@ -49,37 +48,6 @@
 
 (autoload 'cheat "cheat" "" t)
 (autoload 'lisppaste-paste-region "lisppaste" "" t)
-
-;;
-;; xcode (not working at the moment)
-;;
-
-(autoload 'objc-mode "objc-mode" "" t)
-(autoload 'xcode-mode "xcode-mode" "" t)
-
-(add-to-list
- 'auto-mode-alist
- '("^[A-Z]\w*[a-z]\w*[A-Z]\w*(?=\b)\.[h|m]$" . objc-mode))
-
-(add-to-list
- 'auto-mode-alist
- '("[A-Z]\w*[a-z]\w*[A-Z]\w*(?=\b)\.[h|m]$" . xcode-mode))
-
-(add-hook 'objc-mode-common-hook (require 'objc-c-mode))
-
-(define-key objc-mode-map [(meta r)] 'xcode-compile)
-(define-key objc-mode-map [(meta K)] 'xcode-clean)
-
-(add-hook 'c-mode-common-hook
-          (lambda()
-            (local-set-key  [(meta O)] 'ff-find-other-file)))
-(add-hook 'c-mode-common-hook
-          (lambda()
-            (local-set-key (kbd "C-c <right>") 'hs-show-block)
-            (local-set-key (kbd "C-c <left>")  'hs-hide-block)
-            (local-set-key (kbd "C-c <up>")    'hs-hide-all)
-            (local-set-key (kbd "C-c <down>")  'hs-show-all)
-            (hs-minor-mode t)))         ; Hide and show blocks
 
 (provide 'modes)
 ;;; modes.el eof
