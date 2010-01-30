164b0312009805cbaa5a584f13f346cc629c950d
Justin W. Reagor <justinwr@gmail.com>
2010-01-30 18:17:13 -0500

    Changed boldness, again

--- init.el
+++ init.el
@@ -126,7 +126,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 110 :width normal :foundry "apple" :family "Bitstream_Vera_Sans_Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Bitstream_Vera_Sans_Mono")))))
 
 (put 'upcase-region 'disabled nil)
 
