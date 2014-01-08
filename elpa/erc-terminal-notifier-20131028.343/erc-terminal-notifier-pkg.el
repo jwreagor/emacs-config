6b0bc7d7f79b23a5a9baa87450c32283695d6b20
Justin Reagor <justinwr@gmail.com>
2014-01-07 21:52:25 -0500

    Add rust-mode and erc-terminal-notifier (not working atm)

---
+++ elpa/erc-terminal-notifier-20131028.343/erc-terminal-notifier-pkg.el
@@ -0,0 +1,1 @@
+(define-package "erc-terminal-notifier" "20131028.343" "OSX notifications via the terminal-notifier gem for Emacs ERC." (quote nil))
---
+++ elpa/rust-mode-20131214.1941/rust-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rust-mode" "20131214.1941" "A major emacs mode for editing Rust source code" (quote nil))
