eaeb65da003ad9c2c57a76aafc04cdd6f50a7a0f
Justin Reagor <justinwr@gmail.com>
2016-05-30 18:35:10 -0400

    Proper setup for writing lots of rust

---
+++ common/rust-env.el
@@ -0,0 +1,18 @@
+;;;; rust environment
+;;
+
+(autoload 'rust-mode "rust-mode" "" t)
+(autoload 'cargo-minor-mode "cargo-minor-mode" "" t)
+
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+
+(setq racer-cmd "~/.cargo/bin/racer")
+(setq racer-rust-src-path "/Users/justin/.cargo/emacs/rust")
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(provide 'rust-env)
