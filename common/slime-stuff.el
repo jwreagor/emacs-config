87e96634543bc0d110b6642a01458c73c607934d
Justin Reagor <justinwr@gmail.com>
2016-05-30 18:36:38 -0400

    Misc slime stuff in its own temp file

---
+++ common/slime-stuff.el
@@ -0,0 +1,13 @@
+;;;;
+;; previously existing slime stuff
+;;
+
+;;(load (concat dotfiles-dir "quicklisp/slime-helper.el"))
+;; (setq inferior-lisp-program "/usr/local/bin/ccl64")
+
+;; (eval-after-load "slime"
+;;   '(progn
+;;      (define-key slime-mode-map (kbd "C-M-f") 'forward-sexp)
+;;      (define-key slime-mode-map (kbd "C-M-b") 'backward-sexp)
+;;      (define-key slime-mode-map (kbd "C-M-x") 'slime-eval-buffer)
+;;      (setq slime-net-coding-system 'utf-8-unix)))
