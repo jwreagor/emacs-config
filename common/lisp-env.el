d11ab036a358a83ded40a839faa42263bd901708
Justin Reagor <justinwr@gmail.com>
2016-05-30 18:34:34 -0400

    Proper cider setup for writing lots of clojure

--- common/lisp-env.el
+++ common/lisp-env.el
@@ -1,34 +1,51 @@
 ;;;; lisp environment
 ;;
 
-;;;;
-;; slime
-;;
-
-;;(load (concat dotfiles-dir "quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "/usr/local/bin/ccl64")
-
-(add-hook 'clojure-mode
-          (lambda () (enable-paredit-mode)))
-
-(eval-after-load "slime"
-  '(progn
-     (define-key slime-mode-map (kbd "C-M-f") 'forward-sexp)
-     (define-key slime-mode-map (kbd "C-M-b") 'backward-sexp)
-     (define-key slime-mode-map (kbd "C-M-x") 'slime-eval-buffer)
-     (setq slime-net-coding-system 'utf-8-unix)))
+(defun cider-figwheel-repl ()
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/start-figwheel!) ; idempotent
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
 
 (defun hooks/for-lisp-modes ()
   (rainbow-delimiters-mode)
   (paredit-mode +1))
 
-;; (define-key (kbd "C-M-DEL") 'paredit-splice-sexp)
+;;
+;; repl
+;;
 
-(add-hook 'slime-repl-mode-hook 'hooks/for-lisp-modes)
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+(setq cider-repl-use-pretty-printing t)
+(setq cider-repl-use-clojure-font-lock t)
+(setq cider-repl-result-prefix ";; => ")
+(setq cider-repl-wrap-history t)
+(setq cider-repl-history-size 3000)
+(setq cider-show-error-buffer nil)
+
+;;
+;; hooks
+;;
+
+;; (add-hook 'slime-repl-mode-hook 'hooks/for-lisp-modes)
+;;(add-hook 'inferior-lisp-mode-hook 'hooks/for-lisp-modes)
 (add-hook 'lisp-mode-hook 'hooks/for-lisp-modes)
-(add-hook 'inferior-lisp-mode-hook 'hooks/for-lisp-modes)
 (add-hook 'emacs-lisp-mode-hook 'hooks/for-lisp-modes)
 (add-hook 'clojure-mode-hook 'hooks/for-lisp-modes)
+(add-hook 'cider-repl-mode-hook 'hooks/for-lisp-modes)
+(add-hook 'cider-mode-hook #'eldoc-mode)
+(add-hook 'clojure-mode (lambda () (enable-paredit-mode)))
+
+;;
+;; keys
+;;
+
+(global-set-key (kbd "C-c C-f") #'cider-figwheel-repl)
+;; (define-key (kbd "C-M-DEL") 'paredit-splice-sexp)
 
 (require 'lisp-helpers)
 
